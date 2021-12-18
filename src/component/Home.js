63450c2936fb3f5d20ef1c14f737506576d86cd1
AadeshJ-Dev <aadeshj24@gmail.com>
2021-12-18 23:02:45 +0530

    lazy load implemented

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,42 +1,74 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import Nav from './Nav'
 import styled from "styled-components";
 import axios from 'axios';
 import useGetUserList from './useGetUserList'
 
 export default function Home() {
-  const [usersList, setUsersList] = useState('');
-  const [results] = useState(500);
+  // const [usersList, setUsersList] = useState('');
+  const [results, setResults] = useState(40);
 
-  const getUserList = async () => {
-    try {
-      const clearResponse = await axios.get(`https://randomuser.me/api/?results=${results}`);
-      setUsersList(clearResponse.data.results)
-    }
-    catch (error) {
-      console.log(error);
-    }
-  };
+  const {
+    usersList,
+    hasMore,
+    loading,
+    error
+  } = useGetUserList(results)
 
-  useEffect(() => {
-    getUserList();
-  }, [results])
+  const observer = useRef()
+
+  const lastUserElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setResults(prevPageNumber => prevPageNumber + 40)
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
+
+  // const getUserList = async () => {
+  //   try {
+  //     const clearResponse = await axios.get(`https://randomuser.me/api/?results=${results}`);
+  //     setUsersList(clearResponse.data.results)
+  //   }
+  //   catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getUserList();
+  // }, [results])
 
   return (
     <>
       <Nav />
       <HomeContainer>
         <ul>
-          {usersList && usersList.map((eachUser) => {
-            return (
-              <li key={eachUser.cell}>
-                <p>{eachUser.name.first + ' ' + eachUser.name.last}</p>
-                <p><img src={eachUser.picture.thumbnail} alt={eachUser.name.first + ' ' + eachUser.name.last} /></p>
-              </li>
-            )
+          {usersList && usersList.map((eachUser, index) => {
+            if (usersList.length === index + 1) {
+              return (
+                <li ref={lastUserElementRef} key={eachUser.cell}>
+                  <p>{eachUser.name.first + ' ' + eachUser.name.last}</p>
+                  <p><img src={eachUser.picture.thumbnail} alt={eachUser.name.first + ' ' + eachUser.name.last} /></p>
+                </li>
+              )
+            } else {
+              return (
+                <li key={eachUser.cell}>
+                  <p>{eachUser.name.first + ' ' + eachUser.name.last}</p>
+                  <p><img src={eachUser.picture.thumbnail} alt={eachUser.name.first + ' ' + eachUser.name.last} /></p>
+                </li>
+              )
+            }
+
           })}
 
         </ul>
+        <div>{loading && 'Loading...'}</div>
+        <div>{error && 'Error'}</div>
       </HomeContainer>
     </>
   )
@@ -46,11 +78,14 @@ const HomeContainer = styled.div`
 ul{
   padding : 20px;
   list-style : none;
+  overflow:auto;
   li{ 
     padding: 10px;
     border: 1px solid #ccc;
     margin-bottom: 10px;
     overflow:auto;
+    float:left;
+    width:25%;
     p{
       float:left;
       img{
--- src/component/useGetUserList.js
+++ src/component/useGetUserList.js
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export default function useGetUserList(query, results) {
+export default function useGetUserList(results) {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
   const [usersList, setUsersList] = useState([])
   const [hasMore, setHasMore] = useState(false)
 
-  useEffect(() => {
-    setUsersList([])
-  }, [query])
+
 
   useEffect(() => {
     setLoading(true)
@@ -18,11 +16,11 @@ export default function useGetUserList(query, results) {
     axios({
       method: 'GET',
       url: `https://randomuser.me/api/?results=${results}`,
-      params: {results: results },
+      // params: {results: results },
       cancelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
       setUsersList(prevUsers => {
-        return [...new Set([...prevUsers, ...res.data.results.map(b => b.name)])]
+        return [...new Set([...prevUsers, ...res.data.results])]
       })
       setHasMore(res.data.results.length > 0)
       setLoading(false)
@@ -31,7 +29,7 @@ export default function useGetUserList(query, results) {
       setError(true)
     })
     return () => cancel()
-  }, [query, results])
+  }, [results])
 
   return { loading, error, usersList, hasMore }
 }
